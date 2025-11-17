5165f09c093b639b9da35535a240d37c0847e777
MustLuikH7 <mokamartin3@gmail.com>
2025-11-17 09:42:05 +0200

    Add FS module example

--- index.js
+++ index.js
@@ -1,8 +1,2 @@
-console.log("Hello via Bun!");
-const common = require('./common.js')
-
-import common from './common.js'
-common.hello()
-
-import es from './es.js'
-es.hello()
+import fs from 'fs'
+fs.writeFileSync('test.txt', "hello file!")
