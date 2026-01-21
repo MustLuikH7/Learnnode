909bb0312ab849408f4ff8ef92b9e9df524c17b1
MustLuikH7 <mokamartin3@gmail.com>
2026-01-21 13:56:34 +0200

    Add list Example

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,31 @@
 <script setup>
-let message = "Hello vue!";
+import { ref } from 'vue';
+let items = ref(['Sai', 'Piim', 'Viin', 'Õlu'])
+let newItem = ref('')
+function add(){
+  if(newItem.value !== ''){
+  items.value.push(newItem.value.trim())
+  }
+  newItem.value =''
+}
 </script>
 <template>
-  <h1>{{ message }}</h1>
+  <div class="container"> 
+    <div class="field has-addons mt-5">
+      <div class="control is-expanded">
+    <input class="input" type="text" v-model="newItem" @keypress.enter="add">
+  </div>
+  <div class="control">
+        <button class="button is-info" @click="add">
+          Add Item
+        </button>
+      </div>
+      <div class="content">
+        <h1>All Items</h1>
+        <ul>
+            <li v-for="item in items">{{ item }}</li>
+        </ul>
+      </div>
+    </div>
+  </div>
 </template>
