ea10295ae80f646411367e46fe64bac1198f4ac9
MustLuikH7 <mokamartin3@gmail.com>
2026-01-21 14:25:47 +0200

    Add IsDone

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,54 @@
 <script setup>
-import { ref } from 'vue';
-let items = ref(['Sai', 'Piim', 'Viin', 'Õlu'])
+import { computed, ref } from 'vue';
+let i = 1
+let items = ref([
+  { id: i++, text: 'Piim', isDone: true },
+  { id: i++, text: 'Sai', isDone: false },
+  { id: i++, text: 'Viin', isDone: true },
+  { id: i++, text: 'Õlu', isDone: false },
+])
 let newItem = ref('')
-function add(){
-  if(newItem.value !== ''){
-  items.value.push(newItem.value.trim())
+let doneItems = computed(() => {
+  return items.value.filter(i => {
+    return i.isDone
+  })
+})
+function add() {
+  if (newItem.value !== '') {
+    items.value.push({ id: i++, text: newItem.value.trim(), isDone: false })
   }
-  newItem.value =''
+  newItem.value = ''
 }
 </script>
 <template>
-  <div class="container"> 
+  <div class="container">
     <div class="field has-addons mt-5">
       <div class="control is-expanded">
-    <input class="input" type="text" v-model="newItem" @keypress.enter="add">
-  </div>
-  <div class="control">
+        <input class="input" type="text" v-model="newItem" @keypress.enter="add">
+      </div>
+      <div class="control">
         <button class="button is-info" @click="add">
           Add Item
         </button>
       </div>
-      <div class="content">
-        <h1>All Items</h1>
-        <ul>
-            <li v-for="item in items">{{ item }}</li>
-        </ul>
-      </div>
+
+    </div>
+    <div class="content">
+      <h1>All Items</h1>
+      <ul>
+        <li v-for="item in items">{{ item.text }}
+
+          <input type="checkbox" v-model="item.isDone">
+
+        </li>
+      </ul>
+      <h1>Done Items</h1>
+      <ul>
+        <li v-for="item in doneItems">{{ item.text }}
+          <input type="checkbox" v-model="item.isDone">
+
+        </li>
+      </ul>
     </div>
   </div>
 </template>
