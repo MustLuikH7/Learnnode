94f0b3f6dd8e2052a588c9a89260f240af0cfbbf
MustLuikH7 <mokamartin3@gmail.com>
2026-02-04 13:57:23 +0200

    router

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,14 @@
 <script setup>
 
 import { ref } from 'vue';
-import Tabs from './Components/Tabs.vue';
-import ToDo from './Pages/ToDo.vue';
-import Modals from './Pages/Modals.vue';
-let titles = ref([
-  'Todo',
-  'Modals'
+import RouterTabs from './Components/RouterTabs.vue';
 
-]);
-let contents = ref([
-  ToDo,
-  Modals
-
-]); let activeTab = ref(0);
 
 </script>
 
 <template>
-  <Tabs :titles="titles" :active="activeTab" @setActive="activeTab = $event"></Tabs>
-  <div class="container">
-    <component :is="contents[activeTab]"></component>
-  </div>
+  <RouterTabs></RouterTabs>
+  <RouterView></RouterView>
 </template>
 
 <style></style>
