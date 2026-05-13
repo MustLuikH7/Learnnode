020e05ffd802925aa413272d2cd7db535a9b6576
MustLuikH7 <mokamartin3@gmail.com>
2026-05-13 11:34:43 +0300

    add maps2.0

--- src/Pages/Leaflet.vue
+++ src/Pages/Leaflet.vue
@@ -1,45 +1,23 @@
 <script setup>
-import * as L from 'leaflet'
-import 'leaflet/dist/leaflet.css'
-import { onMounted } from 'vue';
 import { ref } from 'vue';
+import LeafletMap from '../Components/LeafletMap.vue';
+let center = ref([59.42699949642546, 24.743485211156457])
+let coords = ref([0, 0]);
 
-onMounted(() => {
-    let coords = ref({ lat: 0, lng: 0 });
-    let location = null
-    if ("geolocation" in navigator) {
-        setInterval(() => {
-            navigator.geolocation.getCurrentPosition((position) => {
-                console.log(position.coords.latitude, position.coords.longitude);
-                coords.value.lat = position.coords.latitude;
-                coords.value.lng = position.coords.longitude;
-                if (location) {
-                    location.setLatLng([coords.value.lat, coords.value.lng]).update()
-                } else {
-                    location = L.marker([coords.value.lat, coords.value.lng]).addTo(map)
-                }
-            })
-
-        }, 1000);
-    } else {
-        /* geolocation IS NOT available */
-    }
-
-    let map = L.map('map').setView([59.42699949642546, 24.743485211156457], 18);
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
-
-})
-
-
-</script>
-
-<template>
-    <div id="map"></div>
-</template>
-<style>
-#map {
-    height: 800px;
+if ("geolocation" in navigator) {
+    setInterval(() => {
+        navigator.geolocation.getCurrentPosition((position) => {
+            console.log(position.coords.latitude, position.coords.longitude);
+            coords.value = [position.coords.latitude, position.coords.longitude];
+        });
+    }, 1000);
+} else {
+    /* geolocation IS NOT available */
 }
-</style>
+</script>
+<template>
+    <button class="button is-primary" @click="center = [57.76153605802084, 26.6240710286201]">Go To Litsi</button>
+    <button class="button is-primary" @click="center = [59.077307157760046, 24.18202527340709]">Go To Tussu</button>
+
+    <LeafletMap :center="center" :zoom="18" :marker="coords"></LeafletMap>
+</template>
